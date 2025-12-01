0b602a087706b7ac222232ac0b62cfb689a4be6f
Samiul821 <smsamiul821@gmail.com>
2025-12-01 16:52:10 +0600

    layout stracture define

--- src/Layout/RootLayout.jsx
+++ src/Layout/RootLayout.jsx
@@ -1,15 +1,19 @@
 import React from "react";
 import { Outlet } from "react-router";
+import Navbar from "../Pages/Shared/Navbar/Navbar";
+import Footer from "../Pages/Shared/Footer/Footer";
 
 const RootLayout = () => {
   return (
-    <div>
-      <header>Navbar</header>
-      <main>
+    <div className="px-[3%] lg:px-[10%]">
+      <header>
+        <Navbar></Navbar>
+      </header>
+      <main className={`min-h-[calc(100vh-150px)] `}>
         <Outlet></Outlet>
       </main>
       <footer>
-        Footer
+        <Footer></Footer>
       </footer>
     </div>
   );
